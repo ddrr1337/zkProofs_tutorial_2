34df06b599c4454e75129f3011c5d84bb3ae8be8
ddrr1337 <incaelum@hotmail.com>
2025-01-08 00:57:51 +0100

    remove oracles

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,9 +5,6 @@ const networkConfig = {
 
   11155111: {
     name: "sepolia",
-    ORACLE_ROUTER_ADDRESS: "0x485c587Cc4a72EA4bD8395ACec6ee0ee2263c1d9",
-    ORACLE_GRID_ADDRESS: "0x34F581f4c417119172c2C0a2572F09071F11e294",
-    USD_ADDRESS: "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8",
     verify: true,
   },
 };
